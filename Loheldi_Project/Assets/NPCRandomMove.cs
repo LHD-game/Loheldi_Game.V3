5ae99470a336ddec513c69e000058d4a43fa2747
BWKAI <atz00j@naver.com>
2023-02-09 14:47:17 +0900

    NPC Ramdom Move 75%

--- Loheldi_Project/Assets/NPCRandomMove.cs
+++ Loheldi_Project/Assets/NPCRandomMove.cs
@@ -7,35 +7,63 @@ public class NPCRandomMove : MonoBehaviour
 
     Rigidbody rigid;
     
-    public float nextMove1;//다음 행동지표를 결정할 변수
+    public float nextMove1;                   //박스 생성 위치값 (1 = x, 2 = y)
     public float nextMove2;
 
-    float time = 5f;
+    float time = 5f;                          //각 행동에 드는 시간
+
+    public float SmoothTime = 0.1f;           //보간도
+    private Vector3 velocity = Vector3.zero;  //NPC가 가질 가속도값
+
+    public GameObject boxorigin;              //박스 프리팹(뭐든 상관 없음)
+    public GameObject Box;                    //생성될 박스(박스 위치로 이동함)
+
+    bool Move = false;                        //현재 이동, 멈춤을 확인할 값
 
     void Awake()
     {
-        rigid = GetComponent<Rigidbody>();
-        Debug.Log("1");
-        Invoke("Thinking", time); // 초기화 함수 안에 넣어서 실행될 때 마다(최초 1회) nextMove변수가 초기화 되도록함
+        nextMove1 = Random.Range(-1f, 2f);    //생성 위치 랜덤
+        nextMove2 = Random.Range(-1f, 2f);
+        Box = Instantiate(boxorigin, new Vector3(this.gameObject.transform.position.x + nextMove1, -5.4f, this.gameObject.transform.position.z + nextMove2), Quaternion.identity);
+        rigid = GetComponent<Rigidbody>();    //↑ NPC주변에 랜덤한 범위에 Box생성
+        Invoke("Stop", time);                 //처음에는 Stop Invoke 실행
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
-        rigid.velocity = new Vector3(nextMove1, rigid.velocity.y, nextMove2); //nextMove 에 0:멈춤 -1:왼쪽 1:오른쪽 으로 이동 
+        if (Move)
+        {
+
+            this.gameObject.transform.position = Vector3.SmoothDamp(transform.position, Box.transform.position, ref velocity, SmoothTime);     //해당위치로 NPC천천히 이동
+            transform.LookAt(new Vector3(Box.transform.position.x, transform.position.y, Box.transform.position.z));                           //Box위치를 보고있기
+            if (Vector3.Distance(this.transform.position, Box.transform.position) <= 0.2f)                                                     //박스위치와 본인위치의 거리를 비교해서 도착하면 자동으로 멈춤
+            {
+                Move = false;                  //움직이지 않음
+                CancelInvoke();                //Move Invoke 정지
+                Invoke("Stop", 0f);            //Stop Invoke 즉시 실행
+            }
+        }
     }
 
-    public void Thinking()
-    {//몬스터가 스스로 생각해서 판단 (-1:왼쪽이동 ,1:오른쪽 이동 ,0:멈춤  으로 3가지 행동을 판단)
+    public void MoveBox()
+    {
+        Move = true;                           //움직임
 
-        Debug.Log("2");
-        //Random.Range : 최소<= 난수 <최대 /범위의 랜덤 수를 생성(최대는 제외이므로 주의해야함)
-        
-        nextMove1 = Random.Range(-1f, 2f);
+        nextMove1 = Random.Range(-1f, 2f);     //생성 위치 랜덤
         nextMove2 = Random.Range(-1f, 2f);
 
-        time = Random.Range(2f, 5f); //생각하는 시간을 랜덤으로 부여 
-        //Think(); : 재귀함수 : 딜레이를 쓰지 않으면 CPU과부화 되므로 재귀함수쓸 때는 항상 주의 ->Think()를 직접 호출하는 대신 Invoke()사용
-        Invoke("Thinking", time); //매개변수로 받은 함수를 time초의 딜레이를 부여하여 재실행 
+        Box.transform.position = new Vector3(this.gameObject.transform.position.x + nextMove1, -5.4f, this.gameObject.transform.position.z + nextMove2);
+                                               //↑ NPC주변에 랜덤한 범위에 Box생성
+        time = Random.Range(5f, 8f);           //움직이는 시간을 랜덤으로 부여 
+        CancelInvoke();                        //Stop Invoke 정지
+        Invoke("Stop", time);                  //time시간 뒤 Stop Invoke 실행 (time 시간동안 이동)
+    }
+    public void Stop()
+    {
+        Move = false;                          //움직이지 않음
+        rigid.velocity = new Vector3(0,0,0);   //가속도 초기화
+        time = Random.Range(5f, 8f);           //멈춰있는 시간을 랜덤으로 부여 
+        CancelInvoke();                        //Move Invoke 정지
+        Invoke("MoveBox", time);               //time시간 뒤 Move Invoke 실행
     }
 }
