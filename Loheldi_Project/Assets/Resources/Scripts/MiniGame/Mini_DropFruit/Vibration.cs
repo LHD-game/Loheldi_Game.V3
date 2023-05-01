1db6c21328fe7da57953149c5bdb1944d8beed15
BWKAI <atz00j@naver.com>
2023-05-01 13:49:42 +0900

    장작 1차

---
+++ Loheldi_Project/Assets/Resources/Scripts/MiniGame/Mini_DropFruit/Vibration.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Vibration
+{
+    public static void Vibrate()
+    {
+        Handheld.Vibrate();
+    }
+}
