6b26311a5074763df53b5c8b391c551c2146a13a
BWKAI <atz00j@naver.com>
2023-05-26 13:09:11 +0900

    하트가 안 줄어드는 버그 해결

--- Loheldi_Project/Assets/Resources/Scripts/MiniGame/Mini_Wood/WoodGameManager.cs
+++ Loheldi_Project/Assets/Resources/Scripts/MiniGame/Mini_Wood/WoodGameManager.cs
@@ -14,6 +14,7 @@ public class WoodGameManager : MonoBehaviour
     public GameObject DifficultyPanel;
     public GameObject GameOverPanel;
     public GameObject PausePanel;
+    public GameObject HPDisablePanel;
 
     //결과 출력
     public Text ResultTxt;
@@ -92,9 +93,21 @@ public class WoodGameManager : MonoBehaviour
 
     public void DifficultySelect()
     {
-        WelcomePanel.SetActive(false);
-        DifficultyPanel.SetActive(true);
-        state = STATE.SELECT;
+        int now_hp = PlayerPrefs.GetInt("HP");
+
+        if (now_hp > 0)  //현재 hp가 0보다 크다면
+        {
+            //hp 1 감소
+            PlayInfoManager.GetHP(-1);
+            WelcomePanel.SetActive(false);
+            DifficultyPanel.SetActive(true);
+            state = STATE.SELECT;
+        }
+        else    //0 이하라면: 게임 플레이 불가
+        {
+            // hp가 부족합니다! 팝업 띄우기
+            HPDisablePanel.SetActive(true);
+        }
     }
 
     public void WoodSet()
